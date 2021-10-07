0b7578f9e71c397cd4a0895eba50619d46f2d466
kimpilo <475789+kimpilo@users.noreply.github.com>
2021-10-07 14:19:30 +0200

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "The update is once again complete!\n";
+print "The update is once again complete!""\n";
+print "Hepp världen!";
 ?>
